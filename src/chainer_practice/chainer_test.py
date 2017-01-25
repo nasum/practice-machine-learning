9a0bb79ee008e730db21c68be80e750f73d70ee9
nasum <tomato.wonder.life@gmail.com>
2017-01-26 01:01:21 +0900

    add comment and chainer study

---
+++ src/chainer_practice/chainer_test.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import Variable, optimizers, Chain
+
+train, test = chainer.datasets.get_mnist(ndim=3)
+
+# 手書き数字データを描画する関数
+def draw_digit(data):
+    size = 28
+    plt.figure(figsize=(2.5, 3))
+
+    X, Y = np.meshgrid(range(size),range(size))
+    Z = data.reshape(size,size)   # convert from vector to 28x28 matrix
+    Z = Z[::-1,:]             # flip vertical
+    plt.xlim(0,27)
+    plt.ylim(0,27)
+    plt.pcolor(X, Y, Z)
+    plt.gray()
+    plt.tick_params(labelbottom="off")
+    plt.tick_params(labelleft="off")
+
+    plt.show()
+
+print('training data')
+print(train.__class__.__name__)
+
+draw_digit(train[0][0][0])
--- src/first_deeplearning/mnist_cnn.py
+++ src/first_deeplearning/mnist_cnn.py
@@ -4,17 +4,21 @@ import chainer.functions as F
 import chainer.links as L
 from chainer import Variable, optimizers, Chain
 
+# データセットを読み込む。trainにトレーニングデータ。testにテストデータ
 train, test = chainer.datasets.get_mnist(ndim=3)
 
 class Model(Chain):
     def __init__(self):
         super(Model, self).__init__(
+            # たたみ込み層
             conv1 = L.Convolution2D(1, 20, 5),
             conv2 = L.Convolution2D(20, 50, 5),
+            # 多層パーセプトロン
             fc1 = L.Linear(800, 500),
             fc2 = L.Linear(500, 10),
         )
     def __call__(self, x, train = True):
+        #ここでネットワークを作っている
         cv1 = self.conv1(x)
         relu = F.relu(cv1)
         h = F.max_pooling_2d(relu, 2)
@@ -22,8 +26,13 @@ class Model(Chain):
         h = F.dropout(F.relu(self.fc1(h)), train = train)
         return self.fc2(h)
 
+# パラメータ付き関数 誤差の計算をしてくれる
 model = L.Classifier(Model())
+
+# GPUの使用を設定
 model.to_gpu()
+
+# 最適化関数の設定
 optimizer = optimizers.MomentumSGD(lr=0.01, momentum=0.9)
 optimizer.setup(model)
 
@@ -41,9 +50,16 @@ for n in range(20):
     for i in chainer.iterators.SerialIterator(train, batchsize, repeat = False):
         x, t = cov(i, batchsize)
 
+        # 購買を０に
         model.zerograds()
+
+        # 損失を計算
         loss = model(x, t)
+
+        # 逆伝搬
         loss.backward()
+
+        # 最適化関数の更新
         optimizer.update()
 
     i = chainer.iterators.SerialIterator(test, batchsize, repeat = False).next()
