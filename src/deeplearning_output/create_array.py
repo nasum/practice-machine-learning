1a0e6238c9639d0d65761401c3a381b38aa880be
nasum <masaya.nasu@crowdworks.co.jp>
2017-01-31 23:24:54 +0900

    edit output

--- src/deeplearning_output/create_array.py
+++ src/deeplearning_output/create_array.py
@@ -1,13 +1,24 @@
-from chainer.datasets import tuple_datase
+from chainer.datasets import tuple_dataset
 from PIL import Image
 import glob
 import numpy as np
 
 def create_array(path, vector):
     files = glob.glob(path +"/*.png")
-    tupple_array = []
-    for file in files:
-        img = np.array( Image.open(file), dtype=np.float32 )
-        tupple_array.append((img, vector))
 
-    return tupple_array
+    imageData = []
+    labelData = []
+    for file in files:
+        img = Image.open(file)
+        r,g,b = img.split()
+        rImgData = np.asarray(np.float32(r)/255.0)
+        gImgData = np.asarray(np.float32(g)/255.0)
+        bImgData = np.asarray(np.float32(b)/255.0)
+        imgData = np.asarray([rImgData, gImgData, bImgData])
+        imageData.append(imgData)
+        labelData.append(np.int32(vector))
+
+    threshold = np.int32(len(imageData)/8*7)
+    train = tuple_dataset.TupleDataset(imageData[0:threshold], labelData[0:threshold])
+
+    return train
--- src/deeplearning_output/main.py
+++ src/deeplearning_output/main.py
@@ -13,31 +13,38 @@ train_0, test_0 = chainer.datasets.get_mnist(ndim=3)
 train = ca.create_array(args[1],1)
 test = ca.create_array(args[2],0)
 
-print(train_0)
-#print(train)
-
 class Model(Chain):
     def __init__(self):
         super(Model, self).__init__(
-            l1 = L.Linear(25600, 100),
-            l2 = L.Linear(100, 100),
-            l3 = L.Linear(100, 10),
+            # たたみ込み層
+            conv1 = L.Convolution2D(3, 20, 5),
+            conv2 = L.Convolution2D(20, 50, 5),
+            # 多層パーセプトロン
+            fc1 = L.Linear(800, 500),
+            fc2 = L.Linear(500, 10),
         )
-    def __call__(self, x):
-        h = F.relu(self.l1(x))
-        h = F.relu(self.l2(h))
-        return self.l3(h)
+    def __call__(self, x, train = True):
+        #ここでネットワークを作っている
+        cv1 = self.conv1(x)
+        relu = F.relu(cv1)
+        h = F.max_pooling_2d(relu, 2)
+        h = F.max_pooling_2d(F.relu(self.conv2(h)), 2)
+        h = F.dropout(F.relu(self.fc1(h)), train = train)
+        return self.fc2(h)
 
+# パラメータ付き関数 誤差の計算をしてくれる
 model = L.Classifier(Model())
+
 optimizer = optimizers.MomentumSGD(lr=0.01, momentum=0.9)
 optimizer.setup(model)
 
-batchsize = 64
+batchsize = 56
 
 def cov(batch, batchsize):
     x = []
     t = []
     for j in range(batchsize):
+        print(batch[j][0][0][0])
         x.append(batch[j][0])
         t.append(batch[j][1])
     return Variable(np.array(x)), Variable(np.array(t))
@@ -46,9 +53,16 @@ for n in range(20):
     for i in chainer.iterators.SerialIterator(train, batchsize, repeat = False):
         x, t = cov(i, batchsize)
 
+        # 勾配を０に
         model.zerograds()
+
+        # 損失を計算
         loss = model(x, t)
+
+        # 逆伝搬
         loss.backward()
+
+        # 最適化関数の更新
         optimizer.update()
 
     i = chainer.iterators.SerialIterator(test, batchsize, repeat = False).next()
